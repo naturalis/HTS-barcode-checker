b95a6353138ec19b4dacf922ba3503cfbee32241
alex-hoogkamer <aqhoogkamer@outlook.com>
2012-11-22 10:33:03 +0100

    map structuur, data toegevoegd.

---
+++ bin/Trim/Trim.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2.7
+'''
+Created on 22 Nov. 2012
+
+Author: Alex Hoogkamer
+E-mail: aqhoogkamer@outlook.com / s1047388@student.hsleiden.nl
+
+this script will trim fastq files based on the phred quality scores.
+'''
+import Bio
---
+++ src/start.sh
@@ -0,0 +1,1 @@
+#deze file start de pipelijn.
