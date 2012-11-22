ba49cd41edac4d0ec8bc328537a0b611b50a0211
bolderink <thomas_bolderink@hotmail.com>
2012-11-22 14:31:39 +0100

    update blast script

--- bin/BLAST/script1.py
+++ bin/BLAST/script1.py
@@ -17,10 +17,24 @@ save_file.close()
 fasta_handle.close()
 
 fasta_handle = open("my_blast.xml")
-blast_record = NCBIXML.parse(fasta_handle)
 
+blast_records = NCBIXML.parse(fasta_handle)
+blast_record = list(blast_records)
+print blast_record
 
+#blast_record = blast_record.next()
 
+#E_VALUE_THRESH = 0.04
+#for alignment in blast_record.alignments:
+#    for hsp in alignment.hsps:
+#        if hsp.expect < E_VALUE_THRESH:
+#            print '****Alignment****'
+#            print 'sequence:', alignment.title
+#            print 'length:'  , alignment.length
+#            print 'e value:' , hsp.expect
+#            print hsp.query[0:75] + '...'
+#            print hsp.match[0:75] + '...'
+#            print hsp.sbjct[0:75] + '...'
 
 
 
