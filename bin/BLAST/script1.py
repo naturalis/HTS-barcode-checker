fb160960ab86f6e23ec681813780384a24ea5a82
alex-hoogkamer <aqhoogkamer@outlook.com>
2012-11-22 16:06:30 +0100

    update Trim en BLAST script

--- bin/BLAST/script1.py
+++ bin/BLAST/script1.py
@@ -1,61 +1,40 @@
 #!/usr/bin/python2.7
 
 #@author: Thomas Bolderink
+#@co-author: Alex Hoogkamer
 
 # blast script for blast -barcode pipeline
-import Bio
 
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
+import os
 
-fasta_string = open ("MID6.fa").read()
+os.system("pwd > a1")
+file2 = open("a1","r")
+a2 = file2.read()
+file2.close
+a2 = a2.strip("\n")
+file1 = "{pwd}/data/test/s_2_1_sequence-trimmed".format(pwd = a2)
+
+fasta_string = open(file1).read()
 fasta_handle = NCBIWWW.qblast("blastn","nt", fasta_string)
 
-save_file = open("my_blast.xml", "w")
-save_file.write(fasta_handle.read())
-save_file.close()
-fasta_handle.close()
-
-fasta_handle = open("my_blast.xml")
-
 blast_records = NCBIXML.parse(fasta_handle)
-blast_record = list(blast_records)
-print blast_record
 
-#blast_record = blast_record.next()
+E_VALUE_THRESH = 0.04
 
-#E_VALUE_THRESH = 0.04
-#for alignment in blast_record.alignments:
-#    for hsp in alignment.hsps:
-#        if hsp.expect < E_VALUE_THRESH:
-#            print '****Alignment****'
-#            print 'sequence:', alignment.title
-#            print 'length:'  , alignment.length
-#            print 'e value:' , hsp.expect
-#            print hsp.query[0:75] + '...'
-#            print hsp.match[0:75] + '...'
-#            print hsp.sbjct[0:75] + '...'
+for blast_record in blast_records:
+    for alignment in blast_record.alignments:
+        for hsp in alignment.hsps:
+            if hsp.expect < E_VALUE_THRESH:
+                print('****Alignment****')
+                print('sequence:', alignment.title)
+                print('length:'  , alignment.length)
+                print('e value:' , hsp.expect)
+                print(hsp.query[0:75] + '...')
+                print(hsp.match[0:75] + '...')
+                print(hsp.sbjct[0:75] + '...')
 
+os.system("rm a1")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("done 50%")
--- bin/Trim/Trim.py
+++ bin/Trim/Trim.py
@@ -31,8 +31,9 @@ for rec in SeqIO.parse(file1, "fastq"):
             pass
     if qualcount < (len(rec.seq)*0.05):
         out_handle = open("{file1}-trimmed".format(file1=file1), "a")
-        SeqIO.write(rec, out_handle, "fastq")
+        SeqIO.write(rec, out_handle, "fasta")
         out_handle.close
     else:
         pass
 os.system("rm a1")
+print("done 25%")
