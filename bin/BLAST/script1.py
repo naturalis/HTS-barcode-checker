4df0c603760ccee09ff37aa3ead1cfe0393c17e1
bolderink <thomas_bolderink@hotmail.com>
2012-11-22 13:22:04 +0100

    blast script

---
+++ bin/BLAST/script1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python2.7
+
+#@author: Thomas Bolderink
+
+# blast script for blast -barcode pipeline
+import Bio
+
+from Bio.Blast import NCBIWWW
+
+fasta_string = open ("MID6.fa").read()
+fasta_handle = NCBIWWW.qblast("blastn","nt", fasta_string)
+
+save_file = open("my_blast.xml", "w")
+save_file.write(fasta_handle.read())
+save_file.close()
+fasta_handle.close()
+
+fasta_handle = open("my_blast.xml")
+fasta_handle.readlines()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
