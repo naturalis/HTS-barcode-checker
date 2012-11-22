e5e2499b23dd4c11e18c13c5bd8e1b4edbe30270
alex-hoogkamer <aqhoogkamer@outlook.com>
2012-11-22 11:25:05 +0100

    script stubs

---
+++ bin/Output/Output.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2.7
+'''
+Created on 22 Nov. 2012
+
+Author: Alex Hoogkamer
+E-mail: aqhoogkamer@outlook.com / s1047388@student.hsleiden.nl
+
+this script wil take the blast results and change them into a more human friendly format.
+'''
+import Bio
---
+++ bin/Quality/Quality control.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2.7
+'''
+Created on 22 Nov. 2012
+
+Author: Alex Hoogkamer
+E-mail: aqhoogkamer@outlook.com / s1047388@student.hsleiden.nl
+
+this script wil filter the blast results on E-value and bit-score.
+
+coverage 95%
+identity 97%
+'''
+import Bio
