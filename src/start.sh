9d4a61efd8fd23d12e1d1c323bd2d1471639dcbd
alex-hoogkamer <aqhoogkamer@outlook.com>
2013-02-07 14:14:03 +0100

    files verwijdert

--- src/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#this file starts the blast -barcode pipeline (scripts: blast.py , trim.py , quality.py , output.py , cites_check).
-#author: Alex Hoogkamer
-python start.py -i ~/workspace/barcode-blast-pipeline/data/gen -o ~/workspace/barcode-blast-pipeline/data/ -ps ~/workspace/barcode-blast-pipeline/bin
