9a4fc3785da15190969ff2841d4be44511edecd8
bolderink <thomas_bolderink@hotmail.com>
2013-02-07 16:04:19 +0100

    pipeline for windows(test)

---
+++ src/pipe.bat
@@ -0,0 +1,1 @@
+python start.py -o C:\Users\thomas\Documents\GitHub\barcode-blast-pipeline\data\my_blast.xml -db nt -ba blastn -ps C:\Users\thomas\Documents\GitHub\barcode-blast-pipeline\bin -i C:\Users\thomas\Documents\GitHub\barcode-blast-pipeline\data\IonXpress
