2dbc785ff6df9e0d62bb7cd79f18ae779f2018e3
Y-Lammers <youri_lammers_88@hotmail.com>
2013-02-26 12:10:18 +0100

    More bug fixes in the CITES_check.py script, not all parameters turned out to be passed on correctly to the subscripts

--- CITES_Check.py
+++ CITES_Check.py
@@ -21,12 +21,12 @@ parser.add_argument('-s', '--hitlist_size', dest='s', type=str,
 			help = 'Enter the size of the hitlist BLAST wil return (default=1)', default='1')
 parser.add_argument('-m', '--megablast', dest='m', action='store_true', 
 			help = 'Use megablast, can only be used in combination with blastn')
-parser.add_argument('-mi', '--min_identity', dest='mi', type=int, 
-			help = 'Enter the minimal identity for BLAST results', default = 97)
-parser.add_argument('-mc', '--min_coverage', dest='mc', type=int, 
-			help = 'Enter the minimal coverage for BLAST results', default = 100)
-parser.add_argument('-me', '--max_evalue', dest='me', type=float, 
-			help = 'Enter the minimal E-value for BLAST results', default = 0.05)
+parser.add_argument('-mi', '--min_identity', dest='mi', type=str, 
+			help = 'Enter the minimal identity for BLAST results', default = '97')
+parser.add_argument('-mc', '--min_coverage', dest='mc', type=str, 
+			help = 'Enter the minimal coverage for BLAST results', default = '100')
+parser.add_argument('-me', '--max_evalue', dest='me', type=str, 
+			help = 'Enter the minimal E-value for BLAST results', default = '0.05')
 parser.add_argument('-b', '--blacklist', metavar='blacklist file', dest='b', type=str,
 			help = 'File containing the blacklisted genbank id\'s', default='')
 parser.add_argument('-c', '--CITES_db', metavar='CITES database file', dest='c', type=str,
@@ -45,9 +45,9 @@ def get_CITES ():
 	from subprocess import call
 	
 	if args.fu == True:
-		path = call(['./Retrieve_CITES.py', '-c', args.c, '-f'], shell=True)
+		path = call(['./Retrieve_CITES.py', '-db', args.c, '-f'])
 	else:
-		path = call(['./Retrieve_CITES.py', '-c', args.c], shell=True)
+		path = call(['./Retrieve_CITES.py', '-db', args.c])
 
 
 def run_Blast ():
@@ -56,9 +56,15 @@ def run_Blast ():
 	# Blast_and_Compare.py script from the shell
 	from subprocess import call
 
-	path = call(['./Blast_and_Compare.py', '-i', args.i, '-o', args.o, '-c', args.c,
-			'-a', args.a, '-d', args.d, '-s', args.s, '-m', args.m,
-			'-mi', args.mi, '-mc', args.mc, '-me', args.me, '-b', args.b], shell=True)
+	if args.m == True:
+		path = call(['./Blast_and_Compare.py', '-i', args.i, '-o', args.o, '-c', args.c,
+			'-a', args.a, '-d', args.d, '-s', args.s, '-m', '-mi', args.mi, '-mc', 
+			args.mc, '-me', args.me, '-b', args.b])
+
+	else:
+		path = call(['./Blast_and_Compare.py', '-i', args.i, '-o', args.o, '-c', args.c,
+			'-a', args.a, '-d', args.d, '-s', args.s, '-mi', args.mi, '-mc', args.mc,
+			'-me', args.me, '-b', args.b])
 
 
 def main ():	
