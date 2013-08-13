b65316dccb05f7ff943406214dc48653ae638c2f
rvosa <rutgeraldo@gmail.com>
2013-08-13 14:31:49 +0200

    adding all files to github pages branch for hosting static files

---
+++ README.md
@@ -0,0 +1,198 @@
+HTS-barcode-checker
+===================
+
+The correct taxonomic identification of internationally traded biological materials is 
+crucial for the effective enforcement of the Convention on International Trade in 
+Endangered Species of Wild Fauna and Flora [CITES](http://cites.org/). This project 
+provides a pipeline that automates the putative taxonomic identification of DNA barcodes 
+(e.g. as generated from confiscated materials) by chaining together the steps of DNA 
+sequence similarity searching in public databases and taxonomic name reconciliation of the 
+names associated with returned, similar sequences with the names listed in the CITES 
+"appendices" (which itemize species and higher taxa in which international trade is 
+restricted).
+
+Disclaimer
+----------
+
+Although the authors of this pipeline have taken care to consider exceptions such as 
+incorrectly annotated sequence records in public databases, taxonomic synonyms, and 
+ambiguities in the CITES appendices themselves, the user is advised that the results of 
+this pipeline can in no way be construed as conclusive evidence for either positive or
+negative taxonomic identification of the contents of biological materials. The pipeline
+and the results it produces are provided for informational purposes only. To emphasize
+this point, we reproduce the disclaimer of the license under which this pipeline is 
+released verbatim, below:
+
+**THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.**
+
+Installation instructions
+-------------------------
+
+**Simple:** For command-line usage, the Python script _HTS-barcode-checker_ is provided in 
+the src folder. Assuming the dependencies (listed below) are satisfied, there are no 
+installation steps, the script can simply be run 'as is' with command-line arguments 
+described below.
+
+**Advanced:** To install the pipeline as a locally-hosted web application, in addition to 
+satisfying the dependencies listed below, the following steps must be taken:
+
+* Install the CPAN module Text::CSV.
+* Place the Perl script _HTS-barcode-checker.cgi_ in a location where it is executable as
+a CGI script.
+* Place the Python script _HTS-barcode-checker_ in a location where it can be executed by
+the web server process.
+* Place the default CITES CSV database _data/CITES\_db.csv_ in a location where it is 
+readable by the web server process.
+
+Given the number of different web server configurations that exist it is best to consult 
+your local system administrator if you don't know how to do this.
+
+Dependencies
+------------
+
+* python2.7 or 3
+* bio-python
+* beautiful-soup
+* requests
+
+General usage
+-------------
+
+The basic command to run the pipeline is:
+
+`HTS-barcode-checker --input_file <in.fa> --output_file <out.csv> --CITES_db <db.csv>`
+
+This command will run BLAST searches of the provided input FASTA file(s) against the NCBI
+nucleotide database (by default), then cross-references the returned taxon IDs with local
+databases of taxon IDs that were obtained by taxonomic name reconciliation of the names 
+listed in CITES appendices with the NCBI taxonomy. Any matches are recorded in the output 
+file, a CSV spreadsheet, which needs to be evaluated further by the user.
+
+The pipeline flags critical issues that need to be investigated. In particular, in cases 
+of taxonomic heterogeneity including CITES-listed species (i.e. multiple distinct species
+matching the same sequence) the pipeline warns about this by emitting a message such as:
+
+`CRITICAL: X out of a total of Y distinct taxa for "query" are CITES-listed`
+
+Where X and Y are counts of distinct taxa and "query" is the input sequence identifier 
+that yielded this result. Care must be taken in the interpretation of such results, as
+they can be a source of both Type I and Type II errors.
+
+Input data
+----------
+
+In a typical use case the input file contains high-throughput DNA sequencing reads for a 
+locus commonly used in DNA barcoding (e.g. COI, matK, rbcL). To limit data volumes the 
+user is advised to consider filtering out duplicate and poor quality reads as well as, 
+possibly, clustering the reads a priori (e.g. using octopus) and picking an exemplar or 
+computing a consensus for each cluster. An example file is provided in the data folder as
+_Test\_data.fasta_.
+
+Full command information
+------------------------
+
+Command line arguments:
+
+	HTS-barcode-checker [-h] [-i fasta file] [-o output file] [-ba algorithm]
+		[-bd database] [-hs HS] [-mb] [-mi MI] [-mc MC] [-me ME]
+		[-bl blacklist file] [-cd CITES database file [CITES database file ...]]
+		[-fd] [-ad] [-l log level]
+
+All command line arguments and options can be provided in short or long form, as listed
+below:
+
+	-h, --help            
+		show help message and exit
+  
+	-i <fasta file>, --input_file <fasta file>
+		input data in FASTA format
+		
+	-o <output file>, --output_file <output file>
+		results file in CSV format. if '-' is provided, output is to STDOUT
+		
+	-cd <db file> [<db file> ...], --CITES_db <db file> [<db file> ...]
+		one or more database (CSV) files with CITES-listed taxon identifiers		
+
+	-ba <algorithm>, --BLAST_algorithm <algorithm>
+		BLAST algorithm to use (optional, default=blastn)
+		
+	-bd <database>, --BLAST_database <database>
+		BLAST database to use (optional, default=nt)
+
+	-mb, --megablast      
+		use megablast, can only be used in combination with blastn (optional)
+		
+	-hs <size>, --hitlist_size <size>
+		number of results BLAST wil return (optional, default=10)
+		
+	-mi <identity>, --min_identity <identity>
+		lowest percentage identity for BLAST results to consider (optional, default=97)
+		
+	-mc <coverage>, --min_coverage <coverage>
+		minimal coverage for BLAST results in number of bases (optional, default=100)
+		
+	-me <e-value>, --max_evalue  <e-value>
+		threshold E-value for BLAST results (optional, default=0.05)
+		
+	-bl <blacklist file>, --blacklist <blacklist file>
+		CSV file containing blacklisted genbank accession numbers (optional)					
+						
+	-fd, --force_download
+		force update of the local CITES database (optional)
+
+	-ad, --avoid_download
+		avoid updating the local CITES database (optional)
+
+	-l <verbosity>, --logging <verbosity>
+		set log level to: debug, info, warning (default) or critical
+
+	-lf <log file>, --log_file <log file>
+		specifies a file to log to. if unset, logging is to STDERR
+
+
+Important options
+-----------------
+
+* **Blacklisted GenBank accessions** Some GenBank accessions are known to have incorrect 
+taxon IDs, which can cause both Type I and Type II errors in this pipeline. To avoid such 
+known, problematic, GenBank accessions, the command line argument `--blacklist <list.csv>` 
+can be provided. An example of what such a file should look like is provided in the data 
+folder as _Blacklist.csv_.
+
+* **Synonyms** Some nodes in the NCBI taxonomy are considered to be synonyms of other such 
+nodes. This, too, has the potential to cause Type I and Type II errors. For known nodes 
+where this is the case, (an) additional CSV spreadsheet(s) can be provided that identifies 
+NCBI synonyms that are also CITES-listed. An example of such an additional file is 
+provided in the data folder as _Reconciled\_names\_db.csv_.
+
+* **Local database updates** Periodically, CITES releases new appendices with new lists of 
+names. In order to stay up to date, this pipeline checks whether such new update are 
+available and downloads and processes the new data if this is the case. This behavior can 
+be influenced by either forcing the download (with `--force_update`) or omitting it (with 
+`--avoid_update`) regardless. Downloading the CITES appendices and updating the local 
+database with their contents is done by the _Retrieve\_CITES.py_ script, which is called
+by the main script and consequently meant for internal use only.
+
+* **Verbosity** The script keeps a log of the different processes in the script. The log 
+file is named similar to the file specified with the `--output_file` parameter, but with 
+the .log extension. With the `--logging parameter` the amount of information written to 
+the log file can be altered. The parameter can be set to: WARNING (default), INFO or DEBUG. 
+WARNING logs only the  messages generated when something is amiss with either blasting 
+sequences or updating the CITES database. This verbosity level is the default. INFO logs 
+the basic steps of the pipeline and any recoverable issues that might occur (similar to 
+WARNING). DEBUG logs everything the pipeline does and is of limited use to the end-user.
+
+License
+-------
+
+This software is released under a BSD-3 license, which is provided as the LICENSE file
+included with this project.
+
---
+++ src/Retrieve_CITES.py
@@ -0,0 +1,394 @@
+#!/usr/bin/env python
+
+# Create a local database containing the CITES appendices
+# Database contains the CITES species names and synonymes based on:
+# and the species ncbi species taxon identifier
+
+
+# import the argparse module to handle the input commands
+# and the logging module to track log messages
+import argparse, logging, os
+
+parser = argparse.ArgumentParser(description = 'Create a table containing the CITES species')
+
+parser.add_argument('-db', '--CITES_db', metavar='CITES database name', dest='db',type=str,
+			help='Name and path to the location for the CITES database', nargs='+')
+parser.add_argument('-f', '--force', dest='f', action='store_true',
+			help='Force updating the CITES database')
+parser.add_argument('-l', '--logging', metavar='log level', dest='l', type=str,
+			help = 'Set log level to: debug, info, warning (default) or critical see readme for more details.', default='warning')
+parser.add_argument('-lf', '--log_file', metavar='log file', dest='lf', type=str,
+			help = 'Path to the log file')
+
+args = parser.parse_args()
+
+
+def download_raw_CITES ():
+	
+	# import the urllib2 module used to download the Cites appendices
+	import urllib2
+
+	# open the url and read the .php webpage
+	logging.debug('Downloading CITES appendix webpage.')	
+	CITES_url = urllib2.urlopen('http://www.cites.org/eng/app/appendices.php')
+	CITES_php = CITES_url.read()
+
+	# return the raw .php file
+	return CITES_php
+
+
+def local_CITES_data ():
+	
+	# open the local CITES database(s) to retrieve the date and path of output file
+	results_dic = {}
+	logging.debug('Trying to open the CITES databases provided by the user.')
+	for path in args.db:
+		logging.debug('Trying to open CITES database %s.' % path)
+		try:
+			for line in open(path, 'r'):
+				line = line.rstrip().split(',')
+				if line[0] == 'Date':
+					results_dic['Date'] = line[1]
+					results_dic['output'] = path
+		except:
+			logging.debug('Could not open CITES database %s, new CITES copy will be writen to this location.' % path)
+			results_dic['output'] = path
+	return results_dic
+
+	
+def clean_cell (cell):
+	
+	# minor function to clean up the html tags and 
+	# formating of the CITES appendix
+	import re, unicodedata
+
+	# Try to remove tags, if not possible return a blank
+	try:
+		cell = str(''.join(cell.findAll(text=True)).encode('ascii','ignore'))
+		regex = re.compile(r'[\n\r\t]')
+		cell = regex.sub('', cell).strip().replace('&nbsp;',' ')
+		cell = re.sub(r'&(#?)(.+?);','',cell)
+	
+		while '  ' in cell:
+			cell = cell.replace('  ', ' ')
+
+		return cell
+
+	except:
+		return ''
+
+
+def parse_php (php_file):
+
+	# import BeautifulSoup to parse the webpage
+	from BeautifulSoup import BeautifulSoup
+	import re
+	
+	# fill this dictionary with all species for the 3
+	# CITES categories
+	CITES_dict = {1:[],2:[],3:[]}
+	
+	# create a dictionary for the CITES footnotes
+	CITES_notes = {}
+
+	# read the CITES web page
+	logging.debug('Parsing the CITES html page.')
+	CITES_page = BeautifulSoup(php_file)
+	
+	data = clean_cell(CITES_page.find('strong'))
+
+	# extract the tables
+	tables = CITES_page.findAll('table')
+
+	# parse through the table and find all cites species
+	# (in bold / italic) and under which category they fit
+	rows = tables[1].findAll('tr')
+	for tr in rows[2:]:
+		cols = tr.findAll('td')
+		count = 1
+		for td in cols:
+			cleaned = clean_cell(td.find('b'))
+			# if the cell is filled, retrieve the
+			# species name and add it to the dictionary
+			if cleaned != '':			
+				if ';' in cleaned: cleaned = cleaned.split(';')[1]
+				CITES_dict[count].append([cleaned,clean_cell(td),[clean_cell(note) for note in td.findAll('a')]])
+
+			count += 1
+
+	# parse through the footnotes and create
+	# a dictionary for each one of the notes
+	logging.debug('Parsing the CITES appendix footnotes.')
+	rows = tables[2].findAll('tr')
+	for tr in rows:
+		notes = tr.findAll('td')
+		CITES_notes[clean_cell(notes[0])] = clean_cell(notes[1])
+		
+	
+	# return a list containing the data, species dictionary and CITES footnotes
+	return [data, CITES_dict, CITES_notes]
+
+
+def TNRS (name):
+	
+	# import the request module to connect to the TNRS api
+	# and deal with the JSON resuls and the time module
+	# to prevent floading of the api
+	import requests, time
+
+	# Send the TNRS request
+	logging.debug('Send TNRS request to server.')
+	TNRS_req = requests.get('http://api.phylotastic.org/tnrs/submit',
+		params={'query':name}, allow_redirects=True)
+
+	redirect_url, time_count = TNRS_req.url, 0
+
+	# send retrieve requests at 5 second intervals till
+	# the api returns the JSON object with the results
+	while redirect_url and time_count < 10:
+
+		# Try to Download the JSON object with the TNRS results.
+		try:
+			retrieve_response = requests.get(redirect_url)
+			retrieve_results = retrieve_response.json()
+		except:
+			retieve_results = []
+		
+		# if the results contains the JSON object
+		# retrieve all accepted names for the species
+		# and return these
+		if u'names' in retrieve_results:
+			logging.debug('Parsing TNRS results.')
+			name_list = [name,[]]
+			names = retrieve_results.get(u'names')
+			try:
+				# grab the synonym names if present
+				for item in names[0]['matches']:
+					if item['sourceId'] == 'NCBI':
+						name_list.append(str(item['uri']).split('/')[-1])
+					synonym = item['acceptedName']
+					
+					# check if the synonym isnt the species / genus name of the query
+					if synonym not in name and synonym != '':
+						if ' ' in name:
+							if len(synonym.split(' ')) >= len(name.split(' ')):
+								name_list[1].append(str(item['acceptedName']))
+						else:
+							if ' ' not in synonym:
+								name_list[1].append(str(item['acceptedName']))						
+			except:
+				pass
+
+			# return the list with species names
+			return name_list
+		
+		# time out before sending the new request
+		# use a counter to keep track of the time, if there is
+		# still no server reply the function will return an empty list
+		time.sleep(5)
+		time_count += 5
+
+	logging.warning('Timeout for species %s.' % name)
+	return [name,[]]
+
+
+def get_taxid (species):
+	
+	# get taxon id based on species name (if not provided by TNRS search)
+
+	# import Entrez module from biopython to connect to the genbank servers
+	from Bio import Entrez
+
+	# correct species name for parsing and set temp email
+	Entrez.email = "CITES_check@gmail.com"
+	species = species.replace(' ', '+').strip()
+	
+	# Do a taxonomy search to determine the number of subtaxa
+	# If there are more subtaxa then NCBI returns by default (20)
+	# do a second search with the retmax parameter set to the 
+	# expected number of taxa.
+	try:
+		# Connect to Entrez to obtain subtree size
+		search = Entrez.esearch(term = (species + ' [subtree]'), db = "taxonomy", retmode = "xml")
+		record = Entrez.read(search)
+		count = record['Count']
+	
+		# is subtree size exceeds 20 the entire tree needs to be redownloaded
+		if count > 20:
+			search = Entrez.esearch(term = (species + ' [subtree]'), db = "taxonomy", retmode = "xml", retmax = count)
+			record = Entrez.read(search)
+
+		# if the tree isnt empty, return the taxon ID list.
+		if record['IdList'] != []:	
+			return record['IdList']
+	except:
+		pass
+
+	return ['empty']
+
+
+def obtain_tax (taxid):
+
+	# a module from Biopython is imported to connect to the Entrez database
+	from Bio import Entrez
+		
+	organism = ''
+
+	# based on the taxid the species and taxonomy are retrieved from the Entrez database
+	try:
+		Entrez.email = "CITES_check@gmail.com"
+		search = Entrez.efetch(db="taxonomy", id= taxid, retmode="xml")
+		record = Entrez.read(search)
+		organism = '\"' + record[0]['ScientificName'] + '\"'
+		handle.close()
+	except:
+		pass
+
+	return organism
+
+
+def combine_sets (CITES_dic, CITES_notes):
+
+	# Expand the CITES information with TNRS synonyms and Taxonomic IDs
+	import sys
+
+	# parse through the different CITES appendixes and
+	# and try to retrieve the TNRS synonyms and NCBI Taxonomic IDs
+	# for each species
+
+	taxon_id_dic, CITES_length, failed = {}, sum([len(CITES_dic[appendix]) for appendix in CITES_dic]), 0
+	total = CITES_length
+
+	print('Total number of CITES entries: %i' % CITES_length)
+	logging.info('Total number of CITES entries: %i.' % CITES_length)
+
+	logging.debug('Parse through the CITES species found per appendix.')
+	for appendix in CITES_dic:
+		logging.debug('Parsing through species found in appendix: %s.' % str(appendix))
+		for cell in CITES_dic[appendix]:
+			# create a list of all lower taxon id's
+			temp_name, temp_taxon_list, count = cell[0].replace(' spp.',''), ['empty'], 0
+			
+			# break when a cell turns out to be empty			
+			if temp_name == '' or temp_name == ' ': 
+				failed += 1
+				continue			
+
+			# grab the TAXON IDs for the species name
+			while temp_taxon_list[0] == 'empty' and count <= 20:
+				temp_taxon_list = get_taxid(temp_name)
+				count += 1
+
+			if temp_taxon_list[0] == 'empty': 
+				logging.warning('No taxon ID found for: %s.' % temp_name)
+				failed += 1
+
+			# if no TAXON ID was found for the name
+			# check if taxon IDs can be obtained for
+			# the species synonyms
+			if temp_taxon_list[0] == 'empty':
+				logging.debug('Looking for synonym species: %s.' % temp_name)
+				temp_taxon_list = []
+				TNRS_data = TNRS(temp_name)
+				if len(TNRS_data) < 3 and len(TNRS_data[1]) > 0:
+					for name in TNRS_data[1]:
+						count, temp_tnrs = 0, ['empty']
+						while temp_tnrs[0] == 'empty' and count <= 20:						
+							temp_tnrs = get_taxid(name)
+							count += 1
+						if temp_tnrs[0] != 'empty': temp_taxon_list += temp_tnrs
+					# print the synomyms for who a taxon id could be found					
+					if len(temp_taxon_list) > 0: 
+						logging.debug('Synonym found for %s, taxon ID(s) = %s.' % (temp_name, ' '.join(temp_taxon_list)))
+			
+			if temp_taxon_list == []: 
+				logging.warning('No synonym found for: %s.' % temp_name)
+				failed += 1
+
+			# expand the taxon_id_dic with the taxid's as
+			# keys and the CITES species / CITES cell and 
+			# taxid linked species as values
+			for taxid in temp_taxon_list:
+				CITES_info = cell[1]			
+				if taxid in taxon_id_dic:
+					if appendix > int(taxon_id_dic[taxid][3]): continue
+				
+				# check if footnotes need to be attached:
+				if len(cell[2]) != 0:
+					for item in cell[2]:
+						if item != ' ' and item != '':
+							CITES_info += (' %s: %s' % (item, CITES_notes[item]))
+	
+				taxon_id_dic[taxid] = [cell[0],'\"' + CITES_info + '\"',obtain_tax(taxid),str(appendix)]
+
+			# print the number of remaining CITES entries to process
+			CITES_length -= 1
+			sys.stdout.write('\r' + str(CITES_length) + ' CITES entries remaining')
+			sys.stdout.flush()
+			
+	
+	logging.info('No taxon ID found for %i out of the %i species' % (failed, total))	
+	
+	return taxon_id_dic
+			
+
+def write_csv (date, taxon_id_dic, file_path):
+
+	# write the CITES results to the database
+	logging.debug('Writing CITES results to %s.' % file_path)
+	db = open(file_path, 'w')
+	db.write('#Date of last update:\nDate,' + date + ',\n#taxon id,CITES species,CITES description,taxon species,CITES appendix\n')
+	for taxid in taxon_id_dic:
+		db.write(','.join([taxid] + taxon_id_dic[taxid]) + '\n')
+	db.close()
+			
+
+def main ():	
+
+	# set log level
+	log_level = getattr(logging, args.l.upper(), None)
+	if not isinstance(log_level, int):
+		raise ValueError('Invalid log level: %s' % loglevel)
+		return
+	logging.basicConfig(filename=os.path.splitext(args.lf)[0]+'.log', format='%(asctime)s - %(levelname)s: %(message)s', level=log_level)
+
+	# retrieve the raw CITES appendix page
+	logging.info('Downloading CITES appendix.')
+	CITES_php = download_raw_CITES()
+
+	# parse through the CITES page and retrieve the
+	# species names
+	logging.info('Parsing CITES entries.')
+	CITES_info = parse_php(CITES_php)
+	
+	# try to open the CITES database and check if the current version
+	# (if there is one) is up to date
+	file_data = local_CITES_data()
+	output_path = file_data['output']
+	logging.debug('Test if the current version of the CITES database is up to date.')
+	try:
+		if CITES_info[0] == file_data['Date'] and args.f != True:
+			logging.info('Local CITES database is up to data.')
+			print('Local CITES database is up to date')
+			return
+	except:
+		pass
+
+	logging.info('Downloading new copy of CITES database.')
+	print('Local CITES database either not present or outdated.\nDownloading new local copy of the CITES database')
+
+	# use TNRS to grab the species synonyms and
+	# taxid if available. Expand the taxids with 
+	# taxids from lower ranked records
+	logging.debug('Get taxon IDs for CITES species.')
+	taxon_id_dic = combine_sets(CITES_info[1], CITES_info[2])
+
+	# write the results to the output location
+	print('\nDownloading and formating of the CITES database completed, writing the new database to: %s' % output_path)
+	logging.debug('Write CITES info to output file %s.' % output_path)
+	write_csv(CITES_info[0], taxon_id_dic, output_path)
+	
+
+if __name__ == "__main__":
+	main()	
+
