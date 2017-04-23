e54d7ed9f533412853f3fe63ca33b0397f945e5b
rvosa <rutgeraldo@gmail.com>
2017-04-23 18:46:57 +0200

    adding installer

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hts_barcode_checker',
+      version='0.1',
+      description='Cross-references HTS barcodes with CITES appendices',
+      url='http://github.com/naturalis/HTS-barcode-checker',
+      author='rvosa',
+      author_email='rutgeraldo@gmail.com',
+      license='MIT',
+      packages=['hts_barcode_checker'],
+      zip_safe=False)
